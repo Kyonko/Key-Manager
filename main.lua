231f64d7112eebe1048af0ec31152ec4d1a2940b
Tyler Gibbons <calaihm@gmail.com>
2011-03-23 17:44:06 +0900

    Initial commit.
    Signed-off-by: Tyler Gibbons <calaihm@gmail.com>

---
+++ main.lua
@@ -0,0 +1,5 @@
+-- Key Manager plugin. Written by Tyler "Scuba Steve 9.0/Khalitz/Kavec/etc" Gibbons
+-- Key distribution function written by Ben "Waffles" Lawrence
+-- Bear with me, the comments are going to get weird. I am/was a bit loopy on minocycline when I wrote this.
+
+
